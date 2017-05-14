960c036080df1e276619dd5c37780d54ab8fff57
Nick Pateman <nick@devoctomy.com>
2017-05-14 12:53:47 +0100

    Started implementing drag dropping of cards

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -9,6 +9,9 @@ public class Card : MonoBehaviour
     #region private objects
 
     private ClickHandler cCHrClickHandler;
+    private Vector3 cVe3StartDragPos;
+    private Boolean cBlnDragging;
+    private float cFltDistance;
 
     #endregion
 
@@ -24,19 +27,49 @@ public class Card : MonoBehaviour
     void Update()
     {
         DeckCard pDCdCard = Deck.CardsByGameObject[this.gameObject];
-        pDCdCard.UpdateTween();
+
+        if (cBlnDragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(cFltDistance);
+            transform.position = rayPoint;
+        }
+        else
+        {
+            pDCdCard.UpdateTween();
+        }
+
 
         cCHrClickHandler.Update();
     }
 
     void OnMouseDown()
     {
+        cVe3StartDragPos = this.gameObject.transform.position;
+        cFltDistance = Vector3.Distance(transform.position, Camera.main.transform.position) - 3.0f;
         cCHrClickHandler.OnMouseDown();
+        cBlnDragging = true;
     }
 
     void OnDrawGizmos()
     {
         //nothing to do
+    }
+
+    void OnMouseEnter()
+    {
+        //highlight the card and any cards above it
+    }
+
+    void OnMouseExit()
+    {
+        //unhighlight the card and any cards above it
+    }
+
+    void OnMouseUp()
+    {
+        this.gameObject.transform.position = cVe3StartDragPos;
+        cBlnDragging = false;
     }
 
     #endregion
